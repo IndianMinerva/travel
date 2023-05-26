1e9c8f8a7bd023bc72536aa6a0be7215b320185b
arun <arun.1202@gmail.com>
2023-05-26 23:20:10 +0200

    Fixed minor defects

--- src/main/java/com/example/travel/controller/CustomerController.java
+++ src/main/java/com/example/travel/controller/CustomerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/customers‚‚‚‚‚")
+@RequestMapping("/customers")
 public class CustomerController {
 
     @Autowired
---
+++ src/main/resources/db/migration/V1__hibernate_sequence.sql
@@ -0,0 +1,2 @@
+CREATE TABLE travel.hibernate_sequence(next_val BIGINT);
+insert into travel.hibernate_sequence values(1);
